7b88e3f56bc1cdc78cdfdfa6b900784fd44a7f2e
Anion3r <>
2023-07-18 21:03:22 +0800

    Change default pagination to limit-offset pagination

--- back-end/Project/settings.py
+++ back-end/Project/settings.py
@@ -144,8 +144,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
-    'PAGE_SIZE': 15,
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DATETIME_FORMAT': "%Y-%m-%d %H:%M:%S",
     'DATE_FORMAT': "%Y-%m-%d",
     'TIME_FORMAT': "%H:%M:%S",

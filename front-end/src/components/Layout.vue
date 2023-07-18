a5dc6238f13f1ee9b451827702e2662a89117210
CORE-TAG <VENUS.STAR.WILLIAM@GMAIL.COM>
2023-07-18 23:55:52 +0800

    add: overview page

---
+++ front-end/src/components/Layout.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>
+        <el-menu class="el-menu-demo" mode="horizontal">
+          <div style="margin-top: 5px">
+            <img src="../assets/logo.png" fit="contain" style="width: 50px" />
+            <img src="../assets/psb.png" fit="contain" style="width: 60px" />
+          </div>
+          <el-menu-item
+            v-for="(item, index) in navText"
+            :key="index"
+            :index="item.id"
+            ><a
+              @click.prevent="anchor(item.name)"
+              style="
+                font-family: 'Helvetica Neue';
+                font-size: 20px;
+                font-weight: bolder;
+                width: 100%;
+                height: 100%;
+                display: flex;
+                color: black;
+              "
+              >{{ item.text }}</a
+            ></el-menu-item
+          >
+        </el-menu>
+      </el-header>
+      <el-main>
+        <Overview id="Overview"></Overview>
+        <News id="News"></News>
+        <div id="Facilities">3</div>
+        <div id="Clubs">4</div>
+        <div id="Routes">5</div>
+        <div id="Map">6</div>
+        <div id="RoomRental">7</div>
+        <div id="Journal">8</div>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Overview from "@/components/Overview/index.vue";
+import News from "@/components/News/index.vue";
+export default {
+  name: "Layout",
+  components: {
+    Overview,
+    News,
+  },
+  data() {
+    return {
+      navText: [
+        { id: 1, name: "Overview", text: "Overview" },
+        { id: 2, name: "News", text: "News" },
+        { id: 3, name: "Facilities", text: "Facilities" },
+        { id: 4, name: "Clubs", text: "Clubs" },
+        { id: 5, name: "Routes", text: "Routes" },
+        { id: 6, name: "Map", text: "Map" },
+        { id: 7, name: "RoomRental", text: "Room Rental" },
+        { id: 8, name: "Journal", text: "Journal" },
+      ],
+    };
+  },
+  methods: {
+    anchor(anchorName) {
+      let anchorElement = document.getElementById(anchorName);
+      if (anchorElement) {
+        anchorElement.scrollIntoView();
+      }
+    },
+  },
+};
+</script>
+
+<style lang="less">
+.el-header {
+  z-index: 2000;
+  position: fixed;
+  padding: 0;
+  width: 100%;
+  .el-menu {
+    background-color: rgba(255, 255, 255, 0.2);
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+}
+.el-main {
+  padding: 0;
+}
+</style>
---
+++ front-end/src/components/Overview/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="block">
+    <el-carousel trigger="click" height="63em">
+      <el-carousel-item v-for="url in images" :key="url">
+        <img :src="url" object-fit="cover" width="100%" height="100%" />
+      </el-carousel-item>
+    </el-carousel>
+    <Musk></Musk>
+  </div>
+</template>
+
+<script>
+import Musk from "@/components/Musk/index.vue";
+import requests from "@/api/index.js";
+
+export default {
+  name: "Overview",
+  components: {
+    Musk,
+  },
+  data() {
+    return {
+      images: [],
+    };
+  },
+  mounted() {
+    console.log(this.images);
+    let that = this;
+    requests({
+      method: "get",
+      url: "overviews",
+    }).then(function (response) {
+      console.log(response.results);
+      that.images = response.results;
+    });
+  },
+  methods: {},
+};
+</script>
+
+<style></style>

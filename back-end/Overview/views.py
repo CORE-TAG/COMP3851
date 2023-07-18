8cae6111e724baa4cd497769ca12784e94bfeac2
Anion3r <>
2023-07-18 21:42:30 +0800

    Change response type to list.

--- back-end/Overview/views.py
+++ back-end/Overview/views.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -9,4 +11,12 @@ class OverviewViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     queryset = Overview.objects.all()
     serializer_class = OverviewSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+
+    def list(self, request, *args, **kwargs):
+        result = super().list(request, *args, **kwargs)
+        data = []
+        for d in result.data:
+            data.append(d.get('image'))
+        result.data = data
+        return result
 

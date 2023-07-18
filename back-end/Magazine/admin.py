71664f7a5f660c44fe67644cee7c6834eb81ad4b
Anion3r <>
2023-07-18 20:53:53 +0800

    Create magazine admin page

--- back-end/Magazine/admin.py
+++ back-end/Magazine/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Magazine
 
-# Register your models here.
+
+class MagazineAdmin(admin.ModelAdmin):
+    list_display = ("title", "type", "cover", "created_time", "update_time")
+
+
+admin.site.register(Magazine, MagazineAdmin)
